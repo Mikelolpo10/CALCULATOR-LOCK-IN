e68516cb4f6703e670f5edd9fccd1d6b91e66c97
Mike Wilson <xzvmike@gmail.com>
2025-06-30 11:59:14 +0700

    TAMBAHIN PEMBATAS KARAKTER DI RESULT

---
+++ index.js
@@ -0,0 +1,93 @@
+let calculate = "0";
+let calculatePast = document.querySelector(".history");
+
+document.querySelectorAll(".button-number").forEach(function (button) {
+  button.addEventListener("click", function () {
+    const number = button.textContent;
+
+    if (calculate === "0") {
+      calculate = number;
+    } else {
+      calculate += number;
+    }
+
+    document.querySelector(".result").textContent = calculate;
+    console.log(calculate);
+  });
+});
+
+document.querySelector(".button-reset").addEventListener("click", function () {
+  if (calculate === "0") {
+    if (calculatePast.textContent !== "0") {
+      calculatePast.textContent = "0";
+    }
+  } else {
+    calculate = "0";
+  }
+
+  document.querySelector(".result").textContent = calculate;
+});
+
+document
+  .querySelector(".button-backspace")
+  .addEventListener("click", function () {
+    if (calculate.length > 1) {
+      calculate = calculate.slice(0, -1);
+    } else {
+      calculate = "0";
+    }
+
+    document.querySelector(".result").textContent = calculate;
+  });
+
+document //BIKIN PERSENTASENYA RELATIF SAMA BILANGAN SEBBELUMNYA
+  .querySelector(".button-percentage")
+  .addEventListener("click", function () {
+    const percentage = this.dataset.value;
+    let lastChar = calculate.slice(-1);
+    let firstChars = calculate.toString();
+
+    if ("+-*/".includes(lastChar)) {
+      calculate = calculate.slice(0, -1) + percentage;
+    } else {
+      try {
+        calculate = (firstChars / 100).toString();
+      } catch (error) {
+        document.querySelector(".result").textContent = "Error";
+      }
+    }
+
+    document.querySelector(".result").textContent = calculate;
+  });
+
+document.querySelectorAll(".button-operation").forEach(function (button) {
+  button.addEventListener("click", function () {
+    const operation = this.dataset.value;
+    let lastChar = calculate.slice(-1);
+
+    if ("+-*/%".includes(lastChar)) {
+      calculate = calculate.slice(0, -1) + operation;
+    } else {
+      calculate += operation;
+    }
+
+    document.querySelector(".result").textContent = calculate;
+  });
+});
+
+document.querySelector(".button-comma").addEventListener("click", function () {
+  calculate += this.dataset.value;
+  document.querySelector(".result").textContent = calculate;
+});
+
+document.querySelector(".button-result").addEventListener("click", function () {
+  try {
+    const result = Function('"use strict"; return (' + calculate + ")")();
+    calculatePast.textContent = calculate;
+    calculate = result.toString();
+
+    document.querySelector(".result").textContent = calculate;
+  } catch (error) {
+    document.querySelector(".result").textContent = "0";
+  }
+});
